7b081b9b51e4db785b37dfcda361a5f41646c859
Maxomann <kay.gonschior@googlemail.com>
2014-04-04 22:58:16 +0200

    rewrote FunctionWrapper to work with std::function

--- SilverbirdRPG/SilverbirdRPG/ksFunction.h
+++ SilverbirdRPG/SilverbirdRPG/ksFunction.h
@@ -15,27 +15,53 @@ namespace kg
 	struct make_index_sequence<0u, Is...> : index_sequence<Is...>{ };
 
 
-
 	template<typename F> class FunctionWrapper;
 
+	//NON MEMBER FUNCTION
+	//FunctionWrapper<decltype(foo)> wrapper( foo );
+	template<class Ret, typename ... Args>
+	class FunctionWrapper<Ret( Args... )>
+	{
+		typedef std::function<Ret( Args... )> FuncContainer;
+		const FuncContainer m_function;
 
-	//Member Function
+		template <std::size_t ... Is>
+		void call( const std::vector<std::shared_ptr<void>>& args,
+				   index_sequence<Is...> )
+		{
+			m_function( *static_cast< typename std::remove_reference<Args...>::type* >(args.at( Is ).get())... );
+		}
+	public:
+		FunctionWrapper( const FuncContainer& func ) : m_function( func )
+		{ }
+
+		void call( const std::vector<std::shared_ptr<void>>& args )
+		{
+			assert( args.size() == sizeof...(Args) );
+			call( args, make_index_sequence<sizeof...(Args)>() );
+		}
+
+	};
+
+	//MEMBER FUNCTION
+	//FunctionWrapper<decltype(&Foo::foo)> wrapper( &Foo::foo );
 	template<class Obj, class Ret, typename ... Args>
 	class FunctionWrapper<Ret( Obj::* )(Args...)>
 	{
-		typedef Ret( Obj::*MemberFunction )(Args...);
-		const MemberFunction m_memberfunction;
+		typedef Ret( Obj::*FuncPasser )(Args...);
+		typedef std::function< Ret( Obj&, Args... ) > FuncContainer;
+		const FuncContainer m_function;
 
 		template <std::size_t ... Is>
 		void call( std::shared_ptr<void>& obj,
 				   const std::vector<std::shared_ptr<void>>& args,
 				   index_sequence<Is...> )
 		{
-			(static_cast< Obj* >(obj.get())->*m_memberfunction)
-				(*static_cast< typename std::remove_reference<Args>::type* >(args.at( Is ).get())...);
+			m_function( *static_cast< Obj* >(obj.get()), *static_cast< typename std::remove_reference<Args...>::type* >(args.at( Is ).get())... );
 		}
 	public:
-		FunctionWrapper( MemberFunction memFn ) : m_memberfunction( memFn )
+		//std::mem_fn( func ) is used to bypass a VisualStudio bug
+		FunctionWrapper( const FuncPasser& func ) : m_function( std::mem_fn( func ) )
 		{ }
 
 		void call( std::shared_ptr<void>& obj,
@@ -43,31 +69,6 @@ namespace kg
 		{
 			assert( args.size() == sizeof...(Args) );
 			call( obj, args, make_index_sequence<sizeof...(Args)>() );
-		}
-
-	};
-
-	//Non member funtion
-	template<class Ret, typename ... Args>
-	class FunctionWrapper<Ret( * )(Args...)>
-	{
-		typedef Ret( *Function )(Args...);
-		const Function m_function;
-
-		template <std::size_t ... Is>
-		void call( const std::vector<std::shared_ptr<void>>& args,
-				   index_sequence<Is...> )
-		{
-			m_function( *static_cast< typename std::remove_reference<Args>::type* >(args.at( Is ).get())... );
-		}
-	public:
-		FunctionWrapper( Function func ) : m_function( func )
-		{ }
-
-		void call( const std::vector<std::shared_ptr<void>>& args )
-		{
-			assert( args.size() == sizeof...(Args) );
-			call( args, make_index_sequence<sizeof...(Args)>() );
 		}
 
 	};
