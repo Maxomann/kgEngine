3e20e3b9478e05d54e8e6d55a9013575b5594ca9
Maxomann <kay.gonschior@googlemail.com>
2014-06-07 16:57:58 +0200

    fixed critical error

--- kgEngine/Script/objectSystem/ksClass.h
+++ kgEngine/Script/objectSystem/ksClass.h
@@ -87,9 +87,9 @@ namespace kg
 		const std::string& getType()const;
 
 		template<class T = void>
-		const std::shared_ptr<T>& getCppInstance()const;
+		const std::shared_ptr<T> getCppInstance()const;
 		template<>
-		const std::shared_ptr<void>& getCppInstance()const;
+		const std::shared_ptr<void> getCppInstance()const;
 
 		//args have to be IN ORDER of signature you want to call
 		// RETURN_VALUE:
@@ -128,7 +128,7 @@ namespace kg
 	}
 
 	template<class T/*=void*/>
-	const std::shared_ptr<T>& kg::ksClassInstance::getCppInstance() const
+	const std::shared_ptr<T> kg::ksClassInstance::getCppInstance() const
 	{
 		if( typeid(T).hash_code() == r_master.getHash() )
 			return std::static_pointer_cast< T >(m_instance);
@@ -138,7 +138,7 @@ namespace kg
 		}
 	}
 	template<>
-	const std::shared_ptr<void>& kg::ksClassInstance::getCppInstance() const
+	const std::shared_ptr<void> kg::ksClassInstance::getCppInstance() const
 	{
 		return m_instance;
 	}
